4e27f92c4750f0e7fdc93ba41184d45d7b8f492d
Isaac <ninjamandalov@gmail.com>
2022-09-18 15:32:40 +0100

    ContractCommand Framework

--- AlathraWar2/src/main/java/me/ShermansWorld/AlathraWar/roles/ContractCommand.java
+++ AlathraWar2/src/main/java/me/ShermansWorld/AlathraWar/roles/ContractCommand.java
@@ -8,13 +8,19 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import me.ShermansWorld.AlathraWar.Main;
+import me.ShermansWorld.AlathraWar.UUIDFetcher;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+
 public class ContractCommand implements CommandExecutor {
 
+	public ContractCommand(final Main plugin) {
+        plugin.getCommand("contract").setExecutor((CommandExecutor) this);
+        plugin.getCommand("contract").setTabCompleter(new CustomRoleTabCompletion());
+    }
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		
@@ -25,6 +31,20 @@ public class ContractCommand implements CommandExecutor {
         Map pData = (Map) Main.rolesData.getData(pID);
 		
         p.sendMessage(ChatColor.YELLOW + "Contract Command" );
+		
+		if(!p.hasPermission("AlathraWar.UseRoles")) {
+            p.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+            return false;
+        }
+		if(args.length == 0 || args[0].equalsIgnoreCase("help")) {
+            p.sendMessage(ChatColor.RED + "! "+ ChatColor.RESET + ChatColor.GOLD + "---- Contracts Help ----");
+            p.sendMessage(ChatColor.RED + "! " + ChatColor.GREEN + "/contract list");
+            return false;
+        }
+		else {
+            p.sendMessage(ChatColor.RED + "Unknown sub-command. Do " + ChatColor.YELLOW + "/contract help" + ChatColor.RED + " for more info.");
+        }
+		
 		return false;
 	}
 }
